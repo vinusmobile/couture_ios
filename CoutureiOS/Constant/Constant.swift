6a7fd8e0965a925bc09283e0828eeb30fbd43b94
vinusmobile <mobile_developer@protonmail.com>
2018-11-13 06:13:38 -0700

    add imageBucketBaseLink as a constant

---
+++ CoutureiOS/Constant/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  CoutureiOS
+//
+//  Created by a on 11/13/18.
+//  Copyright © 2018 a. All rights reserved.
+//
+
+import Foundation
+
+public let ImageBucketBaseUrl: String = "http://image.tmdb.org/t/p/w185/"
